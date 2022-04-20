edf14781aba4f2a6b921f622e690114e9f662bf5
Matheus Palmieri <matheusandrepp@gmail.com>
2022-04-20 00:41:01 -0300

    Version Beta

---
+++ README.md
@@ -0,0 +1,54 @@
+<!-- Título -->
+
+<h1 align="center">User System</h1>
+
+<!-- Descrição -->
+
+<p align="center">🎉Projeto em Desenvolvimento🚀</p>
+
+<p align="center">Desenvolvimento feito em React Native, feito para cadastrar usuários, possibilitando editar e excluir.</p>
+
+<!-- Súmario -->
+
+<p align="center">
+ <a href="#features">Features</a> •
+ <a href="#rodando-a-aplicação">Rodando A Aplicação</a> •
+ <a href="#tecnologias">Tecnologias</a> •
+ <a href="#autor">Autor</a>
+</p>
+
+<!-- <img src="images/imagem.png" width="1366px" align="center"> -->
+
+<!-- Atualizações -->
+
+### Features
+
+- [ ] Editar e Excluir
+- [ ] Estilizar
+
+---
+
+### Rodando A Aplicação
+
+No momento abra o arquivo "index.html", em breve será feito o upload no netfly.
+
+---
+
+### Tecnologias
+
+As seguintes ferramentas foram usadas na construção do projeto:
+
+- [HTML](https://html.com/)
+- [CSS](https://html.com/css/)
+- [JavaScript](https://javascript.com/)
+
+---
+
+### Autor
+
+Create for <b>`Matheus Palmieri`</b>👨‍💻
+
+<br>
+<br>
+
+<p align="center">🎉Projeto em Desenvolvimento🚀</p>
--- javascript/controllers/UserController.js
+++ javascript/controllers/UserController.js
@@ -2,13 +2,26 @@ class UserController{
     constructor(){
         this.addEventBtns()
         this.users = {};
+        this.start()
+    }
+
+    start(){
+        if(!localStorage.getItem("users")){
+            localStorage.setItem("users", "{}")
+        }
+        this.users = JSON.parse(localStorage.getItem("users"))
+        Object.values(this.users).forEach((v)=>{
+            let user = new User(v._id, v._name, v._photo, v._email, v._phone, v._admin, v._password)
+            this.addLine(user)
+        })
     }
 
     addLine(user){
         let tr = document.createElement("tr")
+        tr.dataset.user = JSON.stringify(user)
         tr.innerHTML = `
             <td class='table-id'>${user.getId()}</td>
-            <td class='table-icon'>${user.setPhoto()}</td>
+            <td class='table-icon'><img src="${user.setPhoto()}" alt="Icon"></td>
             <td class='table-name'>${user.getName()}</td>
             <td class='table-email'>${user.getEmail()}</td>
             <td class='table-phone'>${user.getPhone()}</td>
@@ -25,6 +38,30 @@ class UserController{
                 <span class="material-icons-sharp delete-btn">delete</span>
             </td>`;
         document.querySelector(".users tbody").appendChild(tr);
+
+        document.querySelectorAll(".edit-btn")[document.querySelectorAll(".edit-btn").length-1].addEventListener("click", ()=>{
+            document.querySelector(".form-edit").style.display = "flex"
+
+            let userObj = JSON.parse(tr.dataset.user)
+            let user = new User(userObj._id, userObj._name, userObj._photo, userObj._email, userObj._phone, userObj._admin, userObj._password)
+            let formEl = document.querySelector("form.edit")
+            let elements = formEl.elements
+            elements.id.value = user.getId()
+            elements.name.value = user.getName()
+            elements.email.value = user.getEmail()
+            elements.phone.value = user.getPhone()
+            elements.admin.checked = user.getAdmin()
+        })
+
+        document.querySelectorAll(".delete-btn")[document.querySelectorAll(".delete-btn").length-1].addEventListener("click", ()=>{
+            if(confirm("Deseja remover este Usuário?")){
+                let userObj = JSON.parse(tr.dataset.user)
+                let user = new User(userObj._id, userObj._name, userObj._photo, userObj._email, userObj._phone, userObj._admin, userObj._password)
+                delete this.users[user.getId()]
+                localStorage.set("users", JSON.stringify(this.users))
+                tr.replaceWith("")
+            }
+        })
     }
 
     readPhoto(data){
@@ -34,10 +71,15 @@ class UserController{
                 resolve(fr.result)
             })
             fr.addEventListener("error",(e)=>{
-                reject
+                reject(e)
             })
             fr.readAsDataURL(data)
         })
+    }
+
+    attUsers(key, value){
+        this.users[key] = value
+        localStorage.setItem("users", JSON.stringify(this.users))
     }
 
     register(){
@@ -49,37 +91,92 @@ class UserController{
             switch(v.type){
                 case "checkbox":
                     registerData.admin = v.checked
-                    break
+                    break;
                 case "file":
                 default:
                     registerData[v.name] = v.value
-                    break
+                    break;
             }
         })
         if(JSON.stringify(this.users) == JSON.stringify({})){
-            user = new User(0, registerData.name, registerData.photo, registerData.email, registerData.phone, 
+            user = new User(0, registerData.name, "", registerData.email, registerData.phone, 
                 registerData.admin, registerData.password)
         } else {
-            let lastUser = Object.values({"a":"teste","b":"teste2"})[Object.values({"a":"teste","b":"teste2"}).length-1]
-            user = new User(lastUser.getId()+1, registerData.name, registerData.photo, registerData.email, registerData.phone, 
-            registerData.admin, registerData.password)
+            let lastUserObj = Object.values(this.users)[Object.values(this.users).length-1]
+            let lastUser = new User(lastUserObj._id, lastUserObj._name, lastUserObj._email, lastUserObj._phone, lastUserObj._admin, lastUserObj._password)
+            user = new User(lastUser.getId()+1, registerData.name, "", registerData.email, registerData.phone, registerData.admin, registerData.password)
         }
         let fileEl = elements.photo
-        if(fileEl.files.length === 0){
-            user.setPhoto("image/icon.jpg")
+        if(fileEl.files.length == 0){
+            user.setPhoto("img/icon.jpg")
+            this.attUsers(user.getId(), user)
             this.addLine(user)
-            document.querySelector(".form-add").style.display = "none"
-            formEl.reset()
+            this.closeForm(document.querySelector("form.register"), document.querySelector(".form-add"))
         } else {
             this.readPhoto(fileEl.files[0]).then((result)=>{
-                this.addLine(user)
-                document.querySelector(".form-add").style.display = "none"
-                formEl.reset()
                 user.setPhoto(result)
+                this.attUsers(user.getId(), user)
+                this.addLine(user)
+                this.closeForm(document.querySelector("form.register"), document.querySelector(".form-add"))
             }, (e)=>{
                 console.error(e)
             })
         }
+    }
+
+    closeForm(form, formContainer){
+        form.reset();
+        formContainer.style.display = "none"
+    }
+
+    attRows(tr,user){
+        tr.dataset.user = JSON.stringify(user)
+        tr.querySelector('.table-icon img').src= user.getPhoto()
+        tr.querySelector('.table-name').innerHTML = user.getName()
+        tr.querySelector('.table-email').innerHTML= user.getEmail()
+        tr.querySelector('.table-phone').innerHTML= user.getPhone()
+        if(user.getAdmin()){
+          tr.querySelector('table-admin').innerHTML = 'Sim'
+        }else{
+            tr.querySelector('.table-admin').innerHTML = 'Não'
+        }
+      }
+
+    edit(){
+        let formEl = document.querySelector(".form.edit")
+        let elements = formEl.elements
+
+        let selectedUser = [...document.querySelectorAll(".users tr:not(:first-child)")].filter((v)=>{
+            if(JSON.parse(v.dataset.user)._id == elements.id.value){
+                return v
+            }
+        })
+
+        let userObj = JSON.parse(selectedUser[0].dataset.user)
+        userObj._name = elements.name.value
+        userObj._email = elements.email.value
+        userObj._phone = elements.phone.value
+        userObj._admin = elements.admin.value
+        // VERIFICAR
+        let user = new User(userObj._id, userObj._name, "", userObj._email, userObj._phone, userObj._admin, userObj._password)
+
+        let files = elements.photo.files
+        if(files.length == 0){
+            user.setPhoto(userObj._photo)
+            this.attUsers(user.getId(), user)
+            this.attRows(selectedUser[0], user)
+            this.closeForm(document.querySelector("form.edit"), document.querySelector(".form-edit"))
+        } else {
+            this.readPhoto(files[0]).then((result)=>{
+                user.setPhoto(result)
+                this.attUsers(user.getId(), user)
+                this.attRows(selectedUser[0], user)
+                this.closeForm(document.querySelector("form.edit"), document.querySelector(".form-edit"))
+            }, (e)=>{
+                console.error(e)
+            })
+        }
+
     }
 
     addEventBtns(){
@@ -87,17 +184,21 @@ class UserController{
         document.querySelector(".add").addEventListener("click", ()=>{
             document.querySelector(".form-add").style.display = "flex"
         })
-        // Buttons Close
+        // Button Close
         document.querySelectorAll(".close")[0].addEventListener("click", ()=>{
-            document.querySelector(".form-add").style.display = "none"
+            this.closeForm(document.querySelector("form.register"), document.querySelector(".form-add"))
         })
-        
+        // Button Check
         document.querySelectorAll(".check")[0].addEventListener("click", ()=>{
             this.register()
         })
-
+        // Button Close
         document.querySelectorAll(".close")[1].addEventListener("click", ()=>{
-            document.querySelector(".form-edit").style.display = "none"
-        })  
+            this.closeForm(document.querySelector("form.edit"), document.querySelector(".form-edit"))
+        });
+        // Button Edit
+        document.querySelectorAll('.check')[1].addEventListener('click', () => {
+            this.edit();
+        })
     }
 }
